9ba1b469a90514de2709dd084ecaf500c39bd56d
Yuri Barbosa <yrmbarbosa@gmail.com>
2021-04-22 17:51:49 -0300

    Anotação de serviço faltando

--- src/main/java/com/yrmb/apitravel/service/StatisticServer.java
+++ src/main/java/com/yrmb/apitravel/service/StatisticServer.java
@@ -2,18 +2,20 @@ package com.yrmb.apitravel.service;
 
 import com.yrmb.apitravel.model.Statistic;
 import com.yrmb.apitravel.model.Travel;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
 
+@Service
 public class StatisticServer {
 
     public Statistic create(List<Travel> travels) {
 
         var statistics = new Statistic();
 
-        statistics.setCount(travels.stream().count());
+        statistics.setCount(travels.size());
         statistics.setAvg(BigDecimal.valueOf(travels.stream().mapToDouble(t -> t.getAmount().doubleValue()).average().orElse(0.0)).setScale(2, RoundingMode.HALF_UP));
         statistics.setMin(BigDecimal.valueOf(travels.stream().mapToDouble(t -> t.getAmount().doubleValue()).min().orElse(0.0)).setScale(2, RoundingMode.HALF_UP));
         statistics.setMax(BigDecimal.valueOf(travels.stream().mapToDouble(t -> t.getAmount().doubleValue()).max().orElse(0.0)).setScale(2, RoundingMode.HALF_UP));
